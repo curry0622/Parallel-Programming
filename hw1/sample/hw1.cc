e283b869b0c6369dc25c3cb2ad776e39f2df0ffa
curry0622 <kingkazma0112305@gmail.com>
2022-10-05 23:56:07 +0800

    feat: get args

--- hw1/sample/hw1.cc
+++ hw1/sample/hw1.cc
@@ -1,17 +1,22 @@
 #include <cstdio>
 #include <mpi.h>
-int main(int argc, char** argv) {
-	MPI_Init(&argc,&argv);
-	int rank, size;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	MPI_File f;
-	MPI_File_open(MPI_COMM_WORLD, argv[1], MPI_MODE_RDONLY, MPI_INFO_NULL, &f);
-	float data[1];
-	MPI_File_read_at(f, sizeof(float) * rank, data, 1, MPI_FLOAT, MPI_STATUS_IGNORE);
-	printf("rank %d got float: %f\n", rank, data[0]);
 
-	MPI_File_open(MPI_COMM_WORLD, output_filename, MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &output);
-	MPI_File_write_at(f, ...
-	MPI_Finalize();
+int main(int argc, char** argv) {
+    // MPI init
+    int rank, procNum;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &procNum);
+
+    // Get args
+    int arrSize = atoi(argv[1]);
+    char inFileName[] = argv[2];
+    char outFileName[] = argv[3];
+
+    printf("array size: %d\n", arrSize);
+    printf("input file name: %s\n", inFileName);
+    printf("output file name: %s\n", outFileName);
+
+    // MPI finalize
+    MPI_Finalize();
 }
