905977eb69321379f89fb4dcf16e219bb82de6d9
curry0622 <kingkazma0112305@gmail.com>
2022-10-06 01:28:52 +0800

    feat: MPI read file

--- hw1/sample/hw1.cc
+++ hw1/sample/hw1.cc
@@ -15,9 +15,24 @@ int main(int argc, char** argv) {
     char* inFileName = argv[2];
     char* outFileName = argv[3];
 
-    printf("array size: %d\n", arrSize);
-    printf("input file name: %s\n", inFileName);
-    printf("output file name: %s\n", outFileName);
+    // Set handle size
+    int handleSize = 0, avgSize = 0;
+    if(arrSize % procNum != 0) {
+        if(rank != procNum - 1)
+            handleSize = arrSize / procNum + 1;
+        else
+            handleSize = arrSize - (procNum - 1) * (arrSize / procNum + 1);
+        avgSize = arrSize / procNum + 1;
+    } else {
+        handleSize = arrSize / procNum;
+        avgSize = arrSize / procNum;
+    }
+
+    // MPI read file
+    float data[handleSize];
+    MPI_File fin;
+    MPI_File_open(MPI_COMM_WORLD, inFileName, MPI_MODE_RDONLY, MPI_INFO_NULL, &fin);
+    MPI_File_read_at_all(fin, sizeof(float) * rank * avgSize, data, handleSize, MPI_FLOAT, MPI_STATUS_IGNORE);
 
     // MPI finalize
     MPI_Finalize();
