98fad3b16d48632dd3f70390e718a7b0356a6628
curry0622 <kingkazma0112305@gmail.com>
2022-10-07 01:26:09 +0800

    refactor

--- hw1/sample/hw1.cc
+++ hw1/sample/hw1.cc
@@ -5,6 +5,8 @@
 #include <algorithm>
 #include <mpi.h>
 
+#define BUF_SIZE 2000000
+
 int cmp (const void * a, const void * b) {
    float fa = *(float *)a;
    float fb = *(float *)b;
@@ -15,11 +17,10 @@ int getOffset(int rank, int arrSize, int procNum) {
     int offset = 0;
     int left = arrSize % procNum;
     int base = arrSize / procNum;
-    if (rank < left) {
+    if (rank < left)
         offset = rank * (base + 1);
-    } else {
+    else
         offset = left * (base + 1) + (rank - left) * base;
-    }
     return offset;
 }
 
@@ -29,63 +30,56 @@ int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &procNum);
+    printf("[Rank %d] MPI init done\n", rank);
 
     // Get args
     int arrSize = atoi(argv[1]);
     char* inFileName = argv[2];
     char* outFileName = argv[3];
+    printf("[Rank %d] Get args done\n", rank);
 
     // Set handle size
-    int handleSize = arrSize / procNum;
+    int cutSize = arrSize / procNum;
     int left = arrSize % procNum;
-    if (rank < left) {
-        handleSize++;
-    }
+    if (rank < left)
+        cutSize++;
     int offset = getOffset(rank, arrSize, procNum);
-    
-    // int handleSize = 0, avgSize = 0;
-    // if(arrSize % procNum != 0) {
-    //     avgSize = arrSize / procNum + 1;
-    //     if(rank != procNum - 1)
-    //         handleSize = arrSize / procNum + 1;
-    //     else
-    //         handleSize = arrSize - (procNum - 1) * (arrSize / procNum + 1);
-    // } else {
-    //     avgSize = arrSize / procNum;
-    //     handleSize = arrSize / procNum;
-    // }
-    // printf("rank: %d, handleSize: %d\n", rank, handleSize);
+    printf("[Rank %d] Set handle size: %d, offset: %d done\n", rank, cutSize, offset);
 
     // MPI read file
-    float data[handleSize];
+    int handleSize = cutSize;
+    float data[2000000];
+    printf("[Rank %d] declare data done\n", rank);
     MPI_File fin;
     MPI_File_open(MPI_COMM_WORLD, inFileName, MPI_MODE_RDONLY, MPI_INFO_NULL, &fin);
     MPI_File_read_at_all(fin, offset * sizeof(float), data, handleSize, MPI_FLOAT, MPI_STATUS_IGNORE);
     MPI_File_close(&fin);
+    printf("[Rank %d] MPI read file done\n", rank);
 
     // Initial sort
     qsort(data, handleSize, sizeof(float), cmp);
     MPI_Barrier(MPI_COMM_WORLD);
+    printf("[Rank %d] Initial sort done\n", rank);
 
     // Odd-Even-Sort
     bool isSortedAll = false;
     bool isOddStage = true;
     while(!isSortedAll) {
         if(isOddStage)
-            printf("[][][Rank: %d, Odd stage start][][]\n", rank);
+            printf("[Rank %d] Odd stage start\n", rank);
         else
-            printf("[][][Rank: %d, Even stage start][][]\n", rank);
+            printf("[Rank %d] Even stage start\n", rank);
         bool isSorted = true;
         if(rank >= arrSize)
             goto BARRIER;
         if(isOddStage) {
             if(rank % 2 == 0) {
                 if(rank > 0) {
-                    printf("Rank: %d, send handleSize to %d\n", rank, rank - 1);
+                    printf("[Rank: %d] send handleSize to %d\n", rank, rank - 1);
                     MPI_Send(&handleSize, 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD);
-                    printf("Rank: %d, send data to %d\n", rank, rank - 1);
+                    printf("[Rank %d] send data to %d\n", rank, rank - 1);
                     MPI_Send(data , handleSize , MPI_FLOAT , rank - 1 , 0, MPI_COMM_WORLD);
-                    printf("Rank: %d, get handleSize from %d\n", rank, rank - 1);
+                    printf("[Rank %d] get handleSize from %d\n", rank, rank - 1);
                     MPI_Recv(data, handleSize, MPI_FLOAT, rank - 1, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
                 } else {
                     goto BARRIER;
@@ -93,10 +87,10 @@ int main(int argc, char** argv) {
             } else {
                 if(rank < std::min(procNum, arrSize) - 1) {
                     int recvSize;
-                    printf("Rank: %d, get handleSize from %d\n", rank, rank + 1);
+                    printf("[Rank %d] get handleSize from %d\n", rank, rank + 1);
                     MPI_Recv(&recvSize, 1, MPI_INT, rank + 1, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
                     float recvData[recvSize];
-                    printf("Rank: %d, get recvData from %d\n", rank, rank + 1);
+                    printf("[Rank %d] get recvData from %d\n", rank, rank + 1);
                     MPI_Recv(recvData, recvSize, MPI_FLOAT, rank + 1, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
                     if(data[handleSize - 1] > recvData[0]) {
                         isSorted = false;
@@ -105,7 +99,7 @@ int main(int argc, char** argv) {
                         memcpy(merged + handleSize, recvData, recvSize * sizeof(float));
                         qsort(merged, handleSize + recvSize, sizeof(float), cmp);
                         memcpy(data, merged, handleSize * sizeof(float));
-                        printf("Rank: %d, send merged to %d\n", rank, rank + 1);
+                        printf("[Rank %d] send merged to %d\n", rank, rank + 1);
                         MPI_Send(merged + handleSize, recvSize, MPI_FLOAT, rank + 1, 0, MPI_COMM_WORLD);
                         free(merged);
                     } else {
@@ -119,10 +113,10 @@ int main(int argc, char** argv) {
             if(rank % 2 == 0) {
                 if(rank < std::min(procNum, arrSize) - 1) {
                     int recvSize;
-                    printf("Rank: %d, get recvSize from %d\n", rank, rank + 1);
+                    printf("[Rank %d] get recvSize from %d\n", rank, rank + 1);
                     MPI_Recv(&recvSize, 1, MPI_INT, rank + 1, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
                     float recvData[recvSize];
-                    printf("Rank: %d, get recvData from %d\n", rank, rank + 1);
+                    printf("[Rank %d] get recvData from %d\n", rank, rank + 1);
                     MPI_Recv(recvData ,recvSize , MPI_FLOAT, rank + 1, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
                     if(data[handleSize - 1] > recvData[0]) {
                         isSorted = false;
@@ -131,7 +125,7 @@ int main(int argc, char** argv) {
                         memcpy(merged + handleSize, recvData, recvSize * sizeof(float));
                         qsort(merged, handleSize + recvSize, sizeof(float), cmp);
                         memcpy(data, merged, handleSize * sizeof(float));
-                        printf("Rank: %d, send merged to %d\n", rank, rank + 1);
+                        printf("[Rank %d] send merged to %d\n", rank, rank + 1);
                         MPI_Send(merged + handleSize, recvSize, MPI_FLOAT, rank + 1, 0, MPI_COMM_WORLD);
                         free(merged);
                     } else {
@@ -142,11 +136,11 @@ int main(int argc, char** argv) {
                 }
             } else {
                 if(rank > 0) {
-                    printf("Rank: %d, send handleSize to %d\n", rank, rank - 1);
+                    printf("[Rank %d] send handleSize to %d\n", rank, rank - 1);
                     MPI_Send(&handleSize, 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD);
-                    printf("Rank: %d, send data to %d\n", rank, rank - 1);
+                    printf("[Rank %d] send data to %d\n", rank, rank - 1);
                     MPI_Send(data, handleSize, MPI_FLOAT, rank - 1, 0, MPI_COMM_WORLD);
-                    printf("Rank: %d, recv data from %d\n", rank, rank - 1);
+                    printf("[Rank %d] recv data from %d\n", rank, rank - 1);
                     MPI_Recv(data, handleSize, MPI_FLOAT, rank - 1, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
                 } else {
                     goto BARRIER;
@@ -154,39 +148,16 @@ int main(int argc, char** argv) {
             }
         }
         BARRIER:
-        
-        // if(isOddStage)
-        //     printf("[][][Rank: %d, Odd stage finish][][]\n", rank);
-        // else
-        //     printf("[][][Rank: %d, Even stage finish][][]\n", rank);
-        // for(int p = 0; p < procNum; p++) {
-        //     if(rank == p) {
-        //         printf("Rank: %d\n", rank);
-        //         for(int i = 0; i < handleSize; i++) {
-        //             printf("%f ", data[i]);
-        //         }
-        //         printf("\n");
-        //     }
-        // }
         isOddStage = !isOddStage;
         MPI_Allreduce(&isSorted, &isSortedAll, 1, MPI_C_BOOL, MPI_LAND, MPI_COMM_WORLD);
     }
-    
-    printf("Rank: %d, break loop\n", rank);
-    // for(int p = 0; p < procNum; p++) {
-    //     if(rank == p) {
-    //         printf("Rank: %d\n", rank);
-    //         for(int i = 0; i < handleSize; i++) {
-    //             printf("%f ", data[i]);
-    //         }
-    //         printf("\n");
-    //     }
-    // }
+    printf("[Rank %d] break loop\n", rank);
 
-    // MPI wriite file
+    // MPI write file
     MPI_File fout;
     MPI_File_open(MPI_COMM_WORLD, outFileName, MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fout);
     MPI_File_write_at_all(fout, offset * sizeof(float), data, handleSize, MPI_FLOAT, MPI_STATUS_IGNORE);
+    MPI_File_close(&fout);
     
     // MPI finalize
     MPI_Finalize();
