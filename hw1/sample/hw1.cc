c44d7e0e2dd2b538a71c8810f020dce5574b423a
curry0622 <kingkazma0112305@gmail.com>
2022-10-07 00:36:38 +0800

    fix: proc > arr

--- hw1/sample/hw1.cc
+++ hw1/sample/hw1.cc
@@ -2,12 +2,25 @@
 #include <stdlib.h>
 #include <cstring>
 #include <cstdio>
+#include <algorithm>
 #include <mpi.h>
 
 int cmp (const void * a, const void * b) {
    float fa = *(float *)a;
    float fb = *(float *)b;
    return (fa > fb) ? 1 : -1;
+}
+
+int getOffset(int rank, int arrSize, int procNum) {
+    int offset = 0;
+    int left = arrSize % procNum;
+    int base = arrSize / procNum;
+    if (rank < left) {
+        offset = rank * (base + 1);
+    } else {
+        offset = left * (base + 1) + (rank - left) * base;
+    }
+    return offset;
 }
 
 int main(int argc, char** argv) {
@@ -23,76 +36,36 @@ int main(int argc, char** argv) {
     char* outFileName = argv[3];
 
     // Set handle size
-    int handleSize = 0, avgSize = 0;
-    if(arrSize % procNum != 0) {
-        avgSize = arrSize / procNum + 1;
-        if(rank != procNum - 1)
-            handleSize = arrSize / procNum + 1;
-        else
-            handleSize = arrSize - (procNum - 1) * (arrSize / procNum + 1);
-    } else {
-        avgSize = arrSize / procNum;
-        handleSize = arrSize / procNum;
+    int handleSize = arrSize / procNum;
+    int left = arrSize % procNum;
+    if (rank < left) {
+        handleSize++;
     }
+    int offset = getOffset(rank, arrSize, procNum);
+    
+    // int handleSize = 0, avgSize = 0;
+    // if(arrSize % procNum != 0) {
+    //     avgSize = arrSize / procNum + 1;
+    //     if(rank != procNum - 1)
+    //         handleSize = arrSize / procNum + 1;
+    //     else
+    //         handleSize = arrSize - (procNum - 1) * (arrSize / procNum + 1);
+    // } else {
+    //     avgSize = arrSize / procNum;
+    //     handleSize = arrSize / procNum;
+    // }
+    // printf("rank: %d, handleSize: %d\n", rank, handleSize);
 
     // MPI read file
     float data[handleSize];
     MPI_File fin;
     MPI_File_open(MPI_COMM_WORLD, inFileName, MPI_MODE_RDONLY, MPI_INFO_NULL, &fin);
-    MPI_File_read_at_all(fin, sizeof(float) * rank * avgSize, data, handleSize, MPI_FLOAT, MPI_STATUS_IGNORE);
+    MPI_File_read_at_all(fin, offset * sizeof(float), data, handleSize, MPI_FLOAT, MPI_STATUS_IGNORE);
     MPI_File_close(&fin);
 
     // Initial sort
     qsort(data, handleSize, sizeof(float), cmp);
     MPI_Barrier(MPI_COMM_WORLD);
-
-    // // Create odd & even groups
-    // MPI_Group worldGroup, oddGroup, evenGroup;
-    // MPI_Comm oddComm, evenComm;
-    // MPI_Comm_group(MPI_COMM_WORLD, &worldGroup);
-
-    // int oddGroupSize, evenGroupSize;
-    // if(rank % 2 == 0) {
-    //     evenGroupSize = rank == procNum - 1 ? 1 : 2;
-    //     oddGroupSize = rank == 0 ? 1 : 2;
-    // } else {
-    //     evenGroupSize = rank == 0 ? 1 : 2;
-    //     oddGroupSize = rank == procNum - 1 ? 1 : 2;
-    // }
-
-    // int oddGroupRanks[oddGroupSize], evenGroupRanks[evenGroupSize];
-    // if(rank % 2 == 0) {
-    //     if(rank == procNum - 1) {
-    //         evenGroupRanks[0] = rank;
-    //     } else {
-    //         evenGroupRanks[0] = rank;
-    //         evenGroupRanks[1] = rank + 1;
-    //     }
-    //     if(rank == 0) {
-    //         oddGroupRanks[0] = rank;
-    //     } else {
-    //         oddGroupRanks[0] = rank - 1;
-    //         oddGroupRanks[1] = rank;
-    //     }
-    // } else {
-    //     if(rank == 0) {
-    //         evenGroupRanks[0] = rank;
-    //     } else {
-    //         evenGroupRanks[0] = rank - 1;
-    //         evenGroupRanks[1] = rank;
-    //     }
-    //     if(rank == procNum - 1) {
-    //         oddGroupRanks[0] = rank;
-    //     } else {
-    //         oddGroupRanks[0] = rank;
-    //         oddGroupRanks[1] = rank + 1;
-    //     }
-    // }
-
-    // MPI_Group_incl(worldGroup, oddGroupSize, oddGroupRanks, &oddGroup);
-    // MPI_Group_incl(worldGroup, evenGroupSize, evenGroupRanks, &evenGroup);
-    // MPI_Comm_create(MPI_COMM_WORLD, oddGroup, &oddComm);
-    // MPI_Comm_create(MPI_COMM_WORLD, evenGroup, &evenComm);
 
     // Odd-Even-Sort
     bool isSortedAll = false;
@@ -103,6 +76,8 @@ int main(int argc, char** argv) {
         else
             printf("[][][Rank: %d, Even stage start][][]\n", rank);
         bool isSorted = true;
+        if(rank >= arrSize)
+            goto BARRIER;
         if(isOddStage) {
             if(rank % 2 == 0) {
                 if(rank > 0) {
@@ -116,7 +91,7 @@ int main(int argc, char** argv) {
                     goto BARRIER;
                 }
             } else {
-                if(rank < procNum - 1) {
+                if(rank < std::min(procNum, arrSize) - 1) {
                     int recvSize;
                     printf("Rank: %d, get handleSize from %d\n", rank, rank + 1);
                     MPI_Recv(&recvSize, 1, MPI_INT, rank + 1, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
@@ -142,7 +117,7 @@ int main(int argc, char** argv) {
             }
         } else {
             if(rank % 2 == 0) {
-                if(rank < procNum - 1) {
+                if(rank < std::min(procNum, arrSize) - 1) {
                     int recvSize;
                     printf("Rank: %d, get recvSize from %d\n", rank, rank + 1);
                     MPI_Recv(&recvSize, 1, MPI_INT, rank + 1, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
@@ -198,13 +173,20 @@ int main(int argc, char** argv) {
     }
     
     printf("Rank: %d, break loop\n", rank);
+    // for(int p = 0; p < procNum; p++) {
+    //     if(rank == p) {
+    //         printf("Rank: %d\n", rank);
+    //         for(int i = 0; i < handleSize; i++) {
+    //             printf("%f ", data[i]);
+    //         }
+    //         printf("\n");
+    //     }
+    // }
 
     // MPI wriite file
     MPI_File fout;
     MPI_File_open(MPI_COMM_WORLD, outFileName, MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fout);
-    MPI_File_write_at_all(fout, sizeof(float) * rank * avgSize, data, handleSize, MPI_FLOAT, MPI_STATUS_IGNORE);
-    // MPI_File_read_at_all(fin, sizeof(float) * rank * avgSize, data, handleSize, MPI_FLOAT, MPI_STATUS_IGNORE);
-    // MPI_File_close(&fin);
+    MPI_File_write_at_all(fout, offset * sizeof(float), data, handleSize, MPI_FLOAT, MPI_STATUS_IGNORE);
     
     // MPI finalize
     MPI_Finalize();
