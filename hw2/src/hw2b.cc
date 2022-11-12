7d62beecf5c6be331a325653032326581eb0eaf9
curry0622 <kingkazma0112305@gmail.com>
2022-11-12 20:41:00 +0800

    feat: 2b baseline 462.18

--- hw2/src/hw2b.cc
+++ hw2/src/hw2b.cc
@@ -8,91 +8,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <iostream>
-#include <chrono>
 #include <mpi.h>
 #include <omp.h>
+#include <math.h>
+#include <iostream>
 
-// Global variables
-int* image;
-int rank, proc_num, thread_num;
-int iters, width, height, calc_width, calc_height;
-int curr_x_start, curr_y_start, proc_y_start, proc_y_end;
-double left, right, lower, upper, x_step, y_step;
-
-// Thread struct
-struct thread_data {
-    int thread_id;
-    int iter;
-    double runtime;
-};
-
-// Set calculation parameters
-void set_calc_wh() {
-    calc_width = width;
-    calc_height = 1000;
-}
-
-// Thread function
-void* thread_func(void* t_data) {
-    auto start = std::chrono::high_resolution_clock::now();
-    int iter = 0;
-
-    while(true) {
-        iter++;
-        // Declare variables
-        int x_start, x_end, y_start, y_end;
-
-        // Entry section
-        pthread_mutex_t mutex;
-        pthread_mutex_init(&mutex, NULL);
-        pthread_mutex_lock(&mutex);
-
-        // Critical section
-        if(curr_y_start >= proc_y_end) {
-            pthread_mutex_unlock(&mutex);
-            pthread_mutex_destroy(&mutex);
-            break;
-        }
-
-        x_start = curr_x_start;
-        x_end = x_start + calc_width;
-        y_start = curr_y_start;
-        y_end = y_start + calc_height;
-
-        if(x_end > width) {
-            x_end = width;
-        }
-        if(y_end > proc_y_end) {
-            y_end = proc_y_end;
-        }
-
-        curr_x_start = x_end;
-        if(curr_x_start >= width) {
-            curr_x_start = 0;
-            curr_y_start += calc_height;
-        }
-
-        // Exit section
-        pthread_mutex_unlock(&mutex);
-        pthread_mutex_destroy(&mutex);
-
-        // Calculate
-        std::cout << "Rank: " << rank << ", Thread " << ((thread_data*)t_data)->thread_id << " calculating from (" << x_start << ", " << y_start << ") to (" << x_end << ", " << y_end << ")" << std::endl;
-        // calc_mandelbrot_set(x_start, x_end, y_start, y_end);
-    }
-    
-    auto end = std::chrono::high_resolution_clock::now();
-    double thread_time = std::chrono::duration<double, std::milli>(end - start).count() / 1000;
-    ((thread_data*)t_data)->iter = iter;
-    ((thread_data*)t_data)->runtime = thread_time;
-
-    // Exit
-    pthread_exit(NULL);
-}
-
-void write_png(const char* filename, int iters, int width, int height, const int* buffer) {
-    FILE* fp = fopen(filename, "wb");
+void write_png(const char *filename, int iters, int width, int height, const int *buffer, const int size, const int calc_height) {
+    FILE *fp = fopen(filename, "wb");
     assert(fp);
     png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
     assert(png_ptr);
@@ -109,14 +31,15 @@ void write_png(const char* filename, int iters, int width, int height, const int
     for (int y = 0; y < height; ++y) {
         memset(row, 0, row_size);
         for (int x = 0; x < width; ++x) {
-            int p = buffer[(height - 1 - y) * width + x];
+            int tmp = height - 1 - y;
+            int p = buffer[((tmp % size) * calc_height + int(tmp / size)) * width + x];
             png_bytep color = row + x * 3;
             if (p != iters) {
                 if (p & 16) {
                     color[0] = 240;
-                    color[1] = color[2] = p % 16 * 16;
+                    color[1] = color[2] = (p & 15) << 4;
                 } else {
-                    color[0] = p % 16 * 16;
+                    color[0] = (p & 15) << 4;
                 }
             }
         }
@@ -131,76 +54,58 @@ void write_png(const char* filename, int iters, int width, int height, const int
 int main(int argc, char** argv) {
     // MPI init
     MPI_Init(&argc, &argv);
+    int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &proc_num);
-
-    // Detect how many CPUs are available
-    cpu_set_t cpu_set;
-    sched_getaffinity(0, sizeof(cpu_set), &cpu_set);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     // Argument parsing
     assert(argc == 9);
     const char* filename = argv[1];
-    iters = strtol(argv[2], 0, 10);
-    left = strtod(argv[3], 0);
-    right = strtod(argv[4], 0);
-    lower = strtod(argv[5], 0);
-    upper = strtod(argv[6], 0);
-    width = strtol(argv[7], 0, 10);
-    height = strtol(argv[8], 0, 10);
-    std::cout << "Image size: " << width << "x" << height << "=" << width * height << std::endl;
+    int iters = strtol(argv[2], 0, 10);
+    double left = strtod(argv[3], 0);
+    double right = strtod(argv[4], 0);
+    double lower = strtod(argv[5], 0);
+    double upper = strtod(argv[6], 0);
+    int width = strtol(argv[7], 0, 10);
+    int height = strtol(argv[8], 0, 10);
 
-    x_step = (right - left) / width;
-    y_step = (upper - lower) / height;
+    double x_step = (right - left) / width;
+    double y_step = (upper - lower) / height;
 
-    proc_y_start = rank * height / proc_num;
-    proc_y_end = (rank + 1) * height / proc_num;
-    std::cout << "rank: " << rank << ", proc_y_start: " << proc_y_start << ", proc_y_end: " << proc_y_end << ", size: " << proc_y_end - proc_y_start << std::endl;
+    int calc_height = ceil((double)height / size);
 
     // Allocate memory for image
-    image = (int*)malloc(width * height * sizeof(int));
+    int* image = (int*)malloc(width * height * sizeof(int));
+    int* buf = (int*)malloc(width * calc_height * sizeof(int));
     assert(image);
-
-    // Set calc width and height
-    set_calc_wh();
-    curr_x_start = 0;
-    curr_y_start = proc_y_start;
-
-    // Create threads
-    thread_num = CPU_COUNT(&cpu_set);
-    pthread_t threads[thread_num];
-    int thread_id[thread_num];
-    thread_data t_data[thread_num];
-    for(int i = 0; i < thread_num; ++i) {
-        thread_id[i] = i;
-        t_data[i].thread_id = i;
-        int rc = pthread_create(&threads[i], NULL, thread_func, (void*)(t_data + i));
-        assert(rc == 0);
-    }
+    assert(buf);
 
     // Mandelbrot set
-    // calc_mandelbrot_set(0, width, 0, height);
-    for (int j = 0; j < height; ++j) {
+    for (int j = rank, row = 0; j < height; j += size, row++) {
         double y0 = j * y_step + lower;
+#pragma omp parallel for schedule(dynamic, 1)
         for (int i = 0; i < width; ++i) {
-            double x0 = i * ((right - left) / width) + left;
+            double x0 = i * x_step + left;
 
             int repeats = 0;
             double x = 0;
             double y = 0;
-            double lsqr = 0;
-            while (repeats < iters && lsqr < 4) {
+            double length_squared = 0;
+            while (repeats < iters && length_squared < 4) {
                 double temp = x * x - y * y + x0;
                 y = 2 * x * y + y0;
                 x = temp;
-                lsqr = x * x + y * y;
+                length_squared = x * x + y * y;
                 ++repeats;
             }
-            image[j * width + i] = repeats;
+            buf[row * width + i] = repeats;
         }
     }
 
-    // Draw and cleanup
-    write_png(filename, iters, width, height, image);
-    free(image);
+    MPI_Gather(buf, width * calc_height, MPI_INT, image, width * calc_height, MPI_INT, 0, MPI_COMM_WORLD);
+
+    // Draw
+    if(rank == 0) {
+        write_png(filename, iters, width, height, image, size, calc_height);
+    }
 }
