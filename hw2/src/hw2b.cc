df00da6036cbe72da7ece63a4439fa2621a53b0d
curry0622 <kingkazma0112305@gmail.com>
2022-11-12 22:24:48 +0800

    feat: my version 494.26

--- hw2/src/hw2b.cc
+++ hw2/src/hw2b.cc
@@ -1,20 +1,21 @@
+/* without vectorization */
 #ifndef _GNU_SOURCE
 #define _GNU_SOURCE
 #endif
 #define PNG_NO_SETJMP
-#include <sched.h>
 #include <assert.h>
+#include <math.h>
 #include <png.h>
+#include <sched.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <mpi.h>
 #include <omp.h>
-#include <math.h>
 #include <iostream>
 
-void write_png(const char *filename, int iters, int width, int height, const int *buffer, const int size, const int calc_height) {
-    FILE *fp = fopen(filename, "wb");
+void write_png(const char* filename, int iters, int width, int height, int size, int calc_height, const int* buffer) {
+    FILE* fp = fopen(filename, "wb");
     assert(fp);
     png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
     assert(png_ptr);
@@ -28,14 +29,13 @@ void write_png(const char *filename, int iters, int width, int height, const int
     png_set_compression_level(png_ptr, 1);
     size_t row_size = 3 * width * sizeof(png_byte);
     png_bytep row = (png_bytep)malloc(row_size);
-    for (int y = 0; y < height; ++y) {
+    for(int y = height - 1; y >= 0; y--) {
         memset(row, 0, row_size);
-        for (int x = 0; x < width; ++x) {
-            int tmp = height - 1 - y;
-            int p = buffer[((tmp % size) * calc_height + int(tmp / size)) * width + x];
+        for(int x = 0; x < width; x++) {
+            int p = buffer[(y % size * calc_height + y / size) * width + x];
             png_bytep color = row + x * 3;
-            if (p != iters) {
-                if (p & 16) {
+            if(p != iters) {
+                if(p & 16) {
                     color[0] = 240;
                     color[1] = color[2] = (p & 15) << 4;
                 } else {
@@ -51,16 +51,17 @@ void write_png(const char *filename, int iters, int width, int height, const int
     fclose(fp);
 }
 
-int main(int argc, char** argv) {
+
+int main(int argc, char **argv) {
     // MPI init
-    MPI_Init(&argc, &argv);
     int rank, size;
+    MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     // Argument parsing
     assert(argc == 9);
-    const char* filename = argv[1];
+    char *filename = argv[1];
     int iters = strtol(argv[2], 0, 10);
     double left = strtod(argv[3], 0);
     double right = strtod(argv[4], 0);
@@ -71,41 +72,45 @@ int main(int argc, char** argv) {
 
     double x_step = (right - left) / width;
     double y_step = (upper - lower) / height;
-
     int calc_height = ceil((double)height / size);
 
-    // Allocate memory for image
-    int* image = (int*)malloc(width * height * sizeof(int));
-    int* buf = (int*)malloc(width * calc_height * sizeof(int));
-    assert(image);
-    assert(buf);
+    // Memory allocation
+    int *buffer = (int *)malloc(calc_height * width * sizeof(int));
+    int *image = (int *)malloc(size * calc_height * width * sizeof(int));
 
     // Mandelbrot set
-    for (int j = rank, row = 0; j < height; j += size, row++) {
+    // int j = height - 1 - rank, row = 0; j >= 0; j -= size, row++
+    // int j = 0, row = 0; j < height; j += size, row++
+    for(int j = rank, row = 0; j < height; j += size, row++) {
         double y0 = j * y_step + lower;
 #pragma omp parallel for schedule(dynamic, 1)
-        for (int i = 0; i < width; ++i) {
-            double x0 = i * x_step + left;
-
+        for(int i = 0; i < width; ++i) {
+            // Initialization
             int repeats = 0;
-            double x = 0;
-            double y = 0;
-            double length_squared = 0;
-            while (repeats < iters && length_squared < 4) {
+            double x = 0, y = 0, lsqr = 0, x0 = i * x_step + left;
+
+            // Calculate the number of iterations
+            while(repeats < iters && lsqr < 4) {
                 double temp = x * x - y * y + x0;
                 y = 2 * x * y + y0;
                 x = temp;
-                length_squared = x * x + y * y;
-                ++repeats;
+                lsqr = x * x + y * y;
+                repeats++;
             }
-            buf[row * width + i] = repeats;
+
+            // Write buffer
+            buffer[row * width + i] = repeats;
         }
     }
 
-    MPI_Gather(buf, width * calc_height, MPI_INT, image, width * calc_height, MPI_INT, 0, MPI_COMM_WORLD);
+    // MPI gather result
+    MPI_Gather(buffer, calc_height * width, MPI_INT, image, calc_height * width, MPI_INT, 0, MPI_COMM_WORLD);
 
-    // Draw
-    if(rank == 0) {
-        write_png(filename, iters, width, height, image, size, calc_height);
-    }
+    // Draw image and cleanup
+    if (rank == 0)
+        write_png(filename, iters, width, height, size, calc_height, image);
+    free(buffer);
+    free(image);
+    MPI_Finalize();
+    return 0;
 }
