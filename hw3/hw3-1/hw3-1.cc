78f78263b0134db3585601884efef31608a8832e
curry0622 <kingkazma0112305@gmail.com>
2022-11-19 13:51:43 +0800

    feat: hw3-1 55.59

--- hw3/hw3-1/hw3-1.cc
+++ hw3/hw3-1/hw3-1.cc
@@ -83,22 +83,32 @@ void cal(
     int B, int Round, int block_start_x, int block_start_y, int block_width, int block_height) {
     int block_end_x = block_start_x + block_height;
     int block_end_y = block_start_y + block_width;
+    int end_k = (Round + 1) * B > n ? n : (Round + 1) * B;
+
+    // int b_i = block_start_x;
+    // int b_j = block_start_y;
+
+    // int block_internal_start_x = b_i * B;
+    // int block_internal_end_x = (b_i + 1) * B > n ? n : (b_i + 1) * B;
+
+    // int block_internal_start_y = b_j * B;
+    // int block_internal_end_y = (b_j + 1) * B > n ? n : (b_j + 1) * B;
 
     for (int b_i = block_start_x; b_i < block_end_x; ++b_i) {
         for (int b_j = block_start_y; b_j < block_end_y; ++b_j) {
             // To calculate B*B elements in the block (b_i, b_j)
             // For each block, it need to compute B times
-            for (int k = Round * B; k < (Round + 1) * B && k < n; ++k) {
+            int block_internal_start_x = b_i * B;
+            int block_internal_end_x = (b_i + 1) * B;
+            int block_internal_start_y = b_j * B;
+            int block_internal_end_y = (b_j + 1) * B;
+            if (block_internal_end_x > n) block_internal_end_x = n;
+            if (block_internal_end_y > n) block_internal_end_y = n;
+
+            for (int k = Round * B; k < end_k; ++k) {
                 // To calculate original index of elements in the block (b_i, b_j)
                 // For instance, original index of (0,0) in block (1,2) is (2,5) for V=6,B=2
-                int block_internal_start_x = b_i * B;
-                int block_internal_end_x = (b_i + 1) * B;
-                int block_internal_start_y = b_j * B;
-                int block_internal_end_y = (b_j + 1) * B;
-
-                if (block_internal_end_x > n) block_internal_end_x = n;
-                if (block_internal_end_y > n) block_internal_end_y = n;
-
+                #pragma omp parallel for schedule(dynamic, 1)
                 for (int i = block_internal_start_x; i < block_internal_end_x; ++i) {
                     for (int j = block_internal_start_y; j < block_internal_end_y; ++j) {
                         if (Dist[i][k] + Dist[k][j] < Dist[i][j]) {
