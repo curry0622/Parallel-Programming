6b96178673b8ba62ad448010b216ca3bbe47887d
curry0622 <kingkazma0112305@gmail.com>
2022-12-08 20:41:09 +0800

    feat: flatten 2d -> 1d

--- hw3/hw3-2/hw3-2.cu
+++ hw3/hw3-2/hw3-2.cu
@@ -1,69 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <cuda.h>
-#include <iostream>
 
-/* Constants */
 const int INF = ((1 << 30) - 1);
 const int V = 50010;
 
-/* Global variables */
 int n, m; // n: # of vertices, m: # of edges
-int* dist;
+int* h_dist;
 
-//======================
-#define DEV_NO 0
-cudaDeviceProp prop;
-
-/* Convert index */
-int convert_index(int i, int j, int n) {
-    return i * n + j;
+int convert_index(int i, int j, int row_size) {
+    return i * row_size + j;
 }
 
-/* Read input */
+/* Read file input */
 void input(char* infile) {
-    // Read # of vertices and # of edges
+    // Read n and m
     FILE* file = fopen(infile, "rb");
     fread(&n, sizeof(int), 1, file);
     fread(&m, sizeof(int), 1, file);
 
-    // Allocate pinned memory for dist
-    cudaError_t stat = cudaMallocHost((void**)&dist, sizeof(int) * n * n);
-    if(stat != cudaSuccess) {
-        std::cout << "Error: " << cudaGetErrorString(stat) << std::endl;
-        exit(-1);
-    }
+    // Allocate memory for h_dist
+    cudaMallocHost((void**)&h_dist, sizeof(int) * n * n);
 
+    // Initialize h_dist
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            std::cout << i << " " << j << std::endl;
-            std::cout << convert_index(i, j, n) << std::endl;
+            int idx = convert_index(i, j, n);
             if (i == j) {
-                dist[convert_index(i, j, n)] = 0;
+                h_dist[idx] = 0;
             } else {
-                dist[convert_index(i, j, n)] = INF;
+                h_dist[idx] = INF;
             }
         }
     }
 
+    // Read edges
     int pair[3];
     for (int i = 0; i < m; ++i) {
         fread(pair, sizeof(int), 3, file);
-        dist[convert_index(pair[0], pair[1], n)] = pair[2];
+        int idx = convert_index(pair[0], pair[1], n);
+        h_dist[idx] = pair[2];
     }
     fclose(file);
 }
 
+/* Write file output */
 void output(char* outFileName) {
     FILE* outfile = fopen(outFileName, "w");
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             int idx = convert_index(i, j, n);
-            if (dist[idx] >= INF) {
-                dist[idx] = INF;
-            }
+            if(h_dist[idx] >= INF)
+                h_dist[idx] = INF;
         }
-        fwrite(dist + i * n, sizeof(int), n, outfile);
+        fwrite(h_dist + i * n, sizeof(int), n, outfile);
     }
     fclose(outfile);
 }
@@ -72,14 +61,66 @@ int ceil(int a, int b) {
     return (a + b - 1) / b;
 }
 
+void cal(int B, int Round, int block_start_x, int block_start_y, int block_width, int block_height) {
+    int block_end_x = block_start_x + block_width;
+    int block_end_y = block_start_y + block_height;
+    int end_k = (Round + 1) * B > n ? n : (Round + 1) * B;
+
+    for (int b_i = block_start_x; b_i < block_end_x; ++b_i) {
+        for (int b_j = block_start_y; b_j < block_end_y; ++b_j) {
+            // To calculate B*B elements in the block (b_i, b_j)
+            // For each block, it need to compute B times
+            int block_internal_start_x = b_i * B;
+            int block_internal_end_x = (b_i + 1) * B;
+            int block_internal_start_y = b_j * B;
+            int block_internal_end_y = (b_j + 1) * B;
+            if (block_internal_end_x > n) block_internal_end_x = n;
+            if (block_internal_end_y > n) block_internal_end_y = n;
+
+            for (int k = Round * B; k < end_k; ++k) {
+                // To calculate original index of elements in the block (b_i, b_j)
+                // For instance, original index of (0,0) in block (1,2) is (2,5) for V=6,B=2
+                #pragma omp parallel for schedule(dynamic, 1)
+                for (int i = block_internal_start_x; i < block_internal_end_x; ++i) {
+                    for (int j = block_internal_start_y; j < block_internal_end_y; ++j) {
+                        int i_k = convert_index(i, k, n);
+                        int k_j = convert_index(k, j, n);
+                        int i_j = convert_index(i, j, n);
+                        if (h_dist[i_k] + h_dist[k_j] < h_dist[i_j]) {
+                            h_dist[i_j] = h_dist[i_k] + h_dist[k_j];
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+void block_FW(int B) {
+    int round = ceil(n, B);
+    for (int r = 0; r < round; ++r) {
+        fflush(stdout);
+        /* Phase 1*/
+        cal(B, r, r, r, 1, 1);
+
+        /* Phase 2*/
+        cal(B, r, r, 0, 1, r);
+        cal(B, r, r, r + 1, 1, round - r - 1);
+        cal(B, r, 0, r, r, 1);
+        cal(B, r, r + 1, r, round - r - 1, 1);
+
+        /* Phase 3*/
+        cal(B, r, 0, 0, r, r);
+        cal(B, r, 0, r + 1, r, round - r - 1);
+        cal(B, r, r + 1, 0, round - r - 1, r);
+        cal(B, r, r + 1, r + 1, round - r - 1, round - r - 1);
+    }
+}
+
 int main(int argc, char* argv[]) {
     input(argv[1]);
     int B = 512;
-
-    // cudaGetDeviceProperties(&prop, DEV_NO);
-    // printf("maxThreadsPerBlock = %d, sharedMemPerBlock = %d\n", prop.maxThreadsPerBlock, prop.sharedMemPerBlock);
-
-    // block_FW(B);
+    block_FW(B);
     output(argv[2]);
     return 0;
 }
